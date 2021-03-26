3ecf9b28bad6740cc3d46d5ccd0fb82b7b9c60ae
hicham1999 <talbihicham99@gmail.com>
2021-03-26 18:28:11 +0100

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function Banner() {
+    
+    const [movie, setMovie] = useState([]);
+
+    return (
+        <header>
+
+        </header>
+    )
+}
+export default Banner;
